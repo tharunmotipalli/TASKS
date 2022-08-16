466d2b4b623247b3c88645bdb9b15eba0b71207b
tharun <tharun.m@ngpwebsmart.com>
2022-08-16 12:09:38 +0530

    add new files

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hello this is tharun');
+document.getElementById("demo1").innerHTML='hello world';
+function hello(){
+    document.getElementById('demo2').innerHTML="hi I am Tharun"
+};
