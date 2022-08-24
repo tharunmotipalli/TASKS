eab78f110b7a3ff0dc7d403504aba0e9ca370523
tharun <tharun.m@ngpwebsmart.com>
2022-08-24 18:19:51 +0530

    h

--- tasknode1.js
+++ tasknode1.js
@@ -2,13 +2,15 @@ let prompt=require('prompt-sync')(); // install prompt-asyn package using npm
 
 const count=prompt('Enter the no of patients:');//prompt the no of patients
 var i=0;
+console.log('enter the names of patients');
 for (let i=0;i<count;i++){ //for loop for getting data for n count of patients
 let promise1= new Promise(function(myResolve, myReject) {  //create a promise 
-  var rand=(Math.floor(Math.random() * 10) + 1 )*1000
+  let rand=(Math.floor(Math.random() * 10))
+  const name = prompt('Patient  name:');// get the name of the patient using prompt 
   setTimeout(function() {
-    const name = prompt('Patient  name:');// get the name of the patient using prompt 
-    myResolve('Time taken By the Patient:'+ rand/1000+'s'); //print the time taken by the patient
-  }, rand);
+  
+    myResolve('Time taken By "'+name+'":'+ rand+'s'); //print the time taken by the patient
+  }, rand*1000);
 });
 
 async function myfunction(){
