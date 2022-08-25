97e8718a7d74533c33b9e001073ae3468b16ac61
tharun <tharun.m@ngpwebsmart.com>
2022-08-25 11:21:45 +0530

    s

---
+++ task.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return typeof (a + b);
+}
+console.log(add(2, 5));
+function subtract(a, b) {
+    return typeof (a - b);
+}
+console.log(subtract(2.5, 0.5));
+function multiply(a, b) {
+    return typeof (a * b);
+}
+console.log(multiply(2, 5));
+function divide(a, b) {
+    return typeof (a / b);
+}
+console.log(divide(2, 5));
+function concat(a, b) {
+    return typeof (a + b);
+}
+console.log(concat('hello', 'world'));
