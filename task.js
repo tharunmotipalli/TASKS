246cc4ba22d1e91cb4c8f4639b4ccf8938a2336d
tharun <tharun.m@ngpwebsmart.com>
2022-08-25 15:36:18 +0530

    d

--- task.js
+++ task.js
@@ -1,20 +1,26 @@
+var a;
+var b;
 function add(a, b) {
-    return typeof (a + b);
+    return (a + b);
 }
 console.log(add(2, 5));
 function subtract(a, b) {
-    return typeof (a - b);
+    return (a - b);
 }
 console.log(subtract(2.5, 0.5));
 function multiply(a, b) {
-    return typeof (a * b);
+    return (a * b);
 }
 console.log(multiply(2, 5));
 function divide(a, b) {
-    return typeof (a / b);
+    return (a / b);
 }
 console.log(divide(2, 5));
 function concat(a, b) {
-    return typeof (a + b);
+    return (a + b);
 }
 console.log(concat('hello', 'world'));
+function increment(a) {
+    return a++;
+}
+console.log(increment(2));
--- task.ts
+++ task.ts
@@ -1,26 +1,33 @@
+let a;
+let b;
+
 function add(a: number, b: number) {
-    return typeof(a+b);
+    return (a+b);
   }
   
   console.log(add(2,5))
-  function subtract(a:any , b: any) {
-      return typeof(a-b);
+  function subtract(a:number , b: number) {
+      return (a-b);
     }
     
     console.log(subtract(2.5,0.5))
     function multiply(a: number, b: number) {
-        return typeof(a*b);
+        return (a*b);
       }
       
       console.log(multiply(2,5))
 
 
 function divide(a: number, b: number) {
-    return typeof(a/b);
+    return (a/b);
   }
   
   console.log(divide(2,5))
-function concat(a:any,b:any){
-    return typeof(a+b);
+function concat(a:string,b:string){
+    return (a+b);
   }
-  console.log(concat('hello','world'))
+  console.log(concat('hello','world'));
+  function increment(a){
+    return a++;
+}
+console.log(increment(2));
